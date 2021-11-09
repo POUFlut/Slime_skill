dbf823af4b7f29fed541227607e8494b1ffe933d
POUFlut <louis180louis55@gmail.com>
2021-11-09 22:28:19 +0800

    1109

--- connectDB.js
+++ connectDB.js
@@ -1,13 +1,8 @@
-// Import the functions you need from the SDKs you need
+
   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-analytics.js";
   import {getFirestore, collection, addDoc} from "https://www.gstatic.com/firebasejs/9.3.0/firebase-firestore.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
     apiKey: "AIzaSyAJ1Of_t7VOmLf_Q9R6OFKMPRoYAyBYzLY",
     authDomain: "slime-skill-db.firebaseapp.com",
     projectId: "slime-skill-db",
@@ -17,7 +12,18 @@
     measurementId: "G-D2YZY8W121"
   };
 
-  // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
   const db = getFirestore();
+  function add(){
+      try {
+  const docRef = await addDoc(collection(db, "usersss"), {
+    first: "Ada",
+    last: "Lovelace",
+    born: 1815
+  });
+  console.log("Document written with ID: ", docRef.id);
+} catch (e) {
+  console.error("Error adding document: ", e);
+}
+  }
