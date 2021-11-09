450ebd27d2dda0c1e9df46364b087207bacd64a6
POUFlut <louis180louis55@gmail.com>
2021-11-10 05:08:54 +0800

    0508

--- js/getdata.js
+++ js/getdata.js
@@ -1,14 +1,9 @@
 //getData
 function getData() {
-    try {
-        db.collection('char').where('skill', 'array-contain', 'ZB2ZA').get().then(result => {
-            Array.prototype.forEach.call(result.docs, r => {
-                console.log(r.id)
+    db.collection('char').where('skill', 'array-contain', 'ZB2ZA').get().then(result => {
+        Array.prototype.forEach.call(result.docs, r => {
+            console.log(r.id)
                 console.log(r.data())
-            })
         })
-    }
-    catch (e) {
-        console.error("Error adding document: ", e);
-    }
+    })
 }
