e02c7f72e3150186b86d2b7c565052644f5f7072
POUFlut <85081382+POUFlut@users.noreply.github.com>
2021-11-10 05:30:03 +0800

    Update getdata.js

--- js/getdata.js
+++ js/getdata.js
@@ -1,6 +1,7 @@
 //getData
-document.write('<script type="module" src="js/connectDB.js"></script>');
+
 function getData() {
+    document.write('<script type="module" src="js/connectDB.js"></script>');
     db.collection('char').where('skill', 'array-contain', 'ZB2ZA').get().then(result => {
         Array.prototype.forEach.call(result.docs, r => {
             console.log(r.id)
