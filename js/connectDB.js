067dbf7de511956b64de65a59380073ef543124a
POUFlut <louis180louis55@gmail.com>
2021-11-17 19:54:25 +0800

    11/17

--- js/connectDB.js
+++ js/connectDB.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 firebase.initializeApp({
     apiKey: "AIzaSyAJ1Of_t7VOmLf_Q9R6OFKMPRoYAyBYzLY",
     authDomain: "slime-skill-db.firebaseapp.com",
@@ -11,23 +10,3 @@ firebase.initializeApp({
 
 const db = firebase.firestore();
 const storage = firebase.storage();
-=======
-//get firestroe function
-	import { initializeApp } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-app.js";
-	import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-analytics.js";
-	import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-firestore.js";
-	const firebaseConfig = {
-		apiKey: "AIzaSyAJ1Of_t7VOmLf_Q9R6OFKMPRoYAyBYzLY",
-		authDomain: "slime-skill-db.firebaseapp.com",
-		projectId: "slime-skill-db",
-		storageBucket: "slime-skill-db.appspot.com",
-		messagingSenderId: "1054534804180",
-		appId: "1:1054534804180:web:c715ebe31ef3f92d335141",
-		measurementId: "G-D2YZY8W121"
-	};
-	const app = initializeApp(firebaseConfig);
-	const analytics = getAnalytics(app);
-	const db = getFirestore();
-	
-	export { db };
->>>>>>> c8ba7f2e0e25db751076fdc2460188c05743e108
